playax/tatu-tasks

commit d34c3730b18b5f1f37f5895f2314588a74347992

Refactor Slack API authentication
- Remove health check route

web.rb
@@ -8,11 +8,7 @@ WORKSPACE = 'playax'
 
 before do
   @body = request.body.read
-  authenticate_request if request.request_method == 'POST'
-end
-
-get '/' do
-  'I\'m Healthy'
+  halt(401) if !Tatu::SlackAPI.authentic?(request, @body)
 end
 
 # For redifining URL on Slack:
@@ -42,13 +38,6 @@ post '/interaction' do
 end
 
 private
-
-def authenticate_request
-  slack_request_timestamp = request.get_header('HTTP_X_SLACK_REQUEST_TIMESTAMP')
-  slack_signature = request.get_header('HTTP_X_SLACK_SIGNATURE')
-
-  halt(401) unless Tatu::SlackAPI.authenticate_slack(@body, slack_request_timestamp, slack_signature)
-end
 
 def handle_reaction(event)
   item = event['item']

spec/web_spec.rb
@@ -3,13 +3,6 @@ require_relative 'spec_helper'
 require_relative '../web'
 
 RSpec.describe Sinatra::Application do
-  it 'greets' do
-    get '/'
-
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('I\'m Healthy')
-  end
-
   describe 'authentication' do
     context 'when no authentication is given' do
       it 'returns 401' do
@@ -37,7 +30,7 @@ RSpec.describe Sinatra::Application do
   end
 
   describe 'task management' do
-    before { expect(Tatu::SlackAPI).to receive(:authenticate_slack).and_return(true) }
+    before { expect(Tatu::SlackAPI).to receive(:authentic?).and_return(true) }
 
     describe 'creation and deletion' do
       let(:task) { instance_double(Tatu::Task) }

lib/tatu/slack_api.rb
@@ -8,7 +8,10 @@ module Tatu
         json.dig('messages', 0, 'text')
       end
 
-      def authenticate_slack(body, slack_request_timestamp, slack_signature)
+      def authentic?(request, body)
+        slack_request_timestamp = request.get_header('HTTP_X_SLACK_REQUEST_TIMESTAMP')
+        slack_signature = request.get_header('HTTP_X_SLACK_SIGNATURE')
+
         data = ['v0', slack_request_timestamp, body].join(':')
         hexdigest = OpenSSL::HMAC.hexdigest('SHA256', signing_secret, data)
         expected_signature = "v0=#{hexdigest}"

commit d81687afae1cf9c7f3b33689f6ddd0292b641957

Fix bug introduced in d34c373: turns out health check is indeed a requirement for this app to work on GKE

web.rb
@@ -7,8 +7,14 @@ also_reload 'lib/tatu' if development?
 WORKSPACE = 'playax'
 
 before do
+  pass if request.get? && request.path == '/'
+
   @body = request.body.read
   halt(401) if !Tatu::SlackAPI.authentic?(request, @body)
+end
+
+get '/' do
+  'I am healthy'
 end
 
 # For redifining URL on Slack:

spec/web_spec.rb
@@ -3,6 +3,15 @@ require_relative 'spec_helper'
 require_relative '../web'
 
 RSpec.describe Sinatra::Application do
+  describe 'health check' do
+    it 'greets' do
+      get '/'
+
+      expect(last_response).to be_ok
+      expect(last_response.body).to eq('I am healthy')
+    end
+  end
+
   describe 'authentication' do
     context 'when no authentication is given' do
       it 'returns 401' do